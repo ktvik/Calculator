7592a53c46df52134240d600e5fad49dc8837e22
Kai Tore Vik <kaitorevik@kaitore.local>
2016-12-26 23:54:08 +0100

    Initial Commit

---
+++ Calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calculator
+//
+//  Created by Kai Tore Vik on 26/12/2016.
+//  Copyright © 2016 Kai Tore Vik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+ // insert code here...
+ printf("Hello, World!\n");
+    return 0;
+}
